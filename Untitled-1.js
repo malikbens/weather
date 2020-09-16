fb93e2e605d8761673e0df857289be4d22b08c1d
malikbens <66621335+malikbens@users.noreply.github.com>
2020-09-16 08:03:28 +0200

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -9,45 +9,7 @@ const searchLong = document.getElementById('loc_long');
 const prevision = document.querySelector('.prevision')
 
 
-let fetchFunction = function (url) {
-    fetch(url)
-        .then((response) =>
-            response.json().then((data) => {
-                console.log(data);
-                cityLocation.innerHTML = data.city.name;
-                tempValue.innerHTML = Math.floor(data.list[0].main.temp) + "°C";
-                tempDescription.innerHTML = data.list[0].weather[0].description;
-                currentTime.innerHTML = data.list[0].dt_txt;
-                iconId.innerHTML = "<img src = " + data.list[0].weather[0].icon + ".png" + " > ";
-
-
-                let html = "";
-                for (let number = 1; number <= 5; number++) {
-                    html += "<img src = " + data.list[number].weather[0].icon + ".png" + " > " + Math.floor(data.list[number].main.temp) + data.list[number].dt_txt;
-                }
-                prevision.innerHTML = html;
-            }))
-}
-
-
-/* demande de géolocalisation */
-
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(setPosition);
-} else {
-    notification.style.display = "block";
-    notification.innerHTML = "<p>Le navigateur ne supporte pas la géolocalisation</p>";
-};
-
-/* enregistre les coordonnées de l'utilisateur , envoie la requette a http://openweather.com/ ,  */
-function setPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=fr`;
-
-    fetchFunction(apiUrl)
-
-    /* autocomplétion avec une fonctionnalité google */
+function googleAutoComplete(position) {
     var searchInput = 'search_input';
 
     $(document).ready(function () {
@@ -71,20 +33,90 @@ function setPosition(position) {
 
     }
     );
-
-    /* function requete a l'api quand l'utilisateur saisie dans l'input */
-
-
-    let apiCall = function (city) {
-        let searchUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=fr`
-
-        fetchFunction(searchUrl)
-    };
-
-    document.querySelector('form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        let city = document.querySelector('#inputCity').value;
-
-        apiCall(city);
-    });
 }
+
+
+
+function fillUpHtml(data) {
+
+    cityLocation.innerHTML = data.city.name;
+    tempValue.innerHTML = Math.floor(data.list[0].main.temp) + "°C";
+    tempDescription.innerHTML = data.list[0].weather[0].description;
+    currentTime.innerHTML = data.list[0].dt_txt;
+    iconId.innerHTML = "<img src = " + data.list[0].weather[0].icon + ".png" + " > ";
+
+
+    /* boucle pour les icons*/
+    let html = "";
+    for (let number = 1; number <= 5; number++) {
+        html += "<img src = " + data.list[number].weather[0].icon + ".png" + " > " + Math.floor(data.list[number].main.temp) + "°C "+ data.list[number].dt_txt;
+    }
+    prevision.innerHTML = html;
+
+    
+}
+
+
+
+let getWeather = function (url) {
+    fetch(url)
+        .then((response) =>
+            response.json().then((data) => {
+                console.log(data);
+                fillUpHtml(data);
+            }))
+}
+
+
+/* demande de géolocalisation */
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition);
+} else {
+    notification.style.display = "block";
+    notification.innerHTML = "<p>Le navigateur ne supporte pas la géolocalisation</p>";
+   
+};
+
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=fr`;
+
+    getWeather(apiUrl);
+    googleAutoComplete(position);
+
+
+}
+
+
+let apiCall = function (city) {
+    let searchUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=fr`;
+    getWeather(searchUrl);
+};
+
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let city = document.querySelector('#inputCity').value;
+
+    apiCall(city);
+});
+
+function celsiusToFahrenheit(temperature){
+    return(temperature * 9/5) + 32 ;
+}
+
+
+
+
+  
+
+
+tempValue.addEventListener('click',function(){
+    
+})
+
+let degre = parseInt(tempValue.textContent.split("°C")[0])
+    let fahrenheit = celsiusToFahrenheit(degre);
